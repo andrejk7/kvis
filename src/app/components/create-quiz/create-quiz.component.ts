ac7d19559ca044ef9254c1760d89465bf3bc1faf
Andrej Kolarovski <andrej@gsix.me>
2018-10-16 14:54:35 +0200

    added mock quiz, html

--- src/app/components/create-quiz/create-quiz.component.ts
+++ src/app/components/create-quiz/create-quiz.component.ts
@@ -1,5 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+const mockQuiz = {
+  topics: [
+    {
+      name: 'trucks',
+      questions: [
+        {
+          points: 10,
+          question: 'Rubber Duck\'s truck?',
+          answer: 'Mack'
+        },
+        {
+          points: 20,
+          question: 'Jerry Reed\'s truck?',
+          answer: 'Kenworth w900'
+        },
+        {
+          points: 30,
+          question: 'Duel truck?',
+          answer: 'Peterbilt 351'
+        },
+        {
+          points: 40,
+          question: 'Ugliest Ukrainian truck?',
+          answer: 'KrAZ 255'
+        },
+        {
+          points: 50,
+          question: 'Dr. alcohol. Kolaroff concert truck?',
+          answer: 'DAF XF'
+        }
+      ]
+    },
+  ]
+};
+
 @Component({
   selector: 'app-create-quiz',
   templateUrl: './create-quiz.component.html',
@@ -8,8 +43,5 @@ import { Component, OnInit } from '@angular/core';
 export class CreateQuizComponent implements OnInit {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
