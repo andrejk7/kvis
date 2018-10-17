42104f8767400f2cff68525cd32a18cd59e9235b
Andrej Kolarovski <andrej@gsix.me>
2018-10-17 13:53:00 +0200

    new question form logic

--- src/app/components/new-question-form/new-question-form.component.ts
+++ src/app/components/new-question-form/new-question-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Question } from '../../../types/question';
+import { Topic } from '../../../types/topic';
 
 @Component({
   selector: 'app-new-question-form',
@@ -7,12 +8,28 @@ import { Question } from '../../../types/question';
   styleUrls: ['./new-question-form.component.css']
 })
 export class NewQuestionFormComponent implements OnInit {
+  @Input() topic: Topic;
   public question: Question;
 
   constructor() { }
 
   ngOnInit() {
-    this.question = new Question;
+    this.initializeEmptyQuestion();
   }
 
+  initializeEmptyQuestion = () => {
+    this.question = new Question;
+    this.initializeDefaultQuestionPoints();
+  }
+
+  initializeDefaultQuestionPoints = () => {
+    // question's index + 1 when it'll be pushed to topic, multiplied by 10
+    this.question.points = (this.topic.questions.length + 1) * 10;
+  }
+
+  addQuestion = (event: any) => {
+    event.preventDefault();
+    this.topic.questions.push(this.question);
+    this.initializeEmptyQuestion();
+  }
 }
--- src/app/components/new-topic-form/new-topic-form.component.ts
+++ src/app/components/new-topic-form/new-topic-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Quiz } from '../../../types/quiz';
+import { Topic } from '../../../types/topic';
+
+const MIN_TOPIC_QUESTIONS_COUNT = 5;
 
 @Component({
   selector: 'app-new-topic-form',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-topic-form.component.css']
 })
 export class NewTopicFormComponent implements OnInit {
+  @Input() quiz: Quiz;
 
-  constructor() { }
+  public topic: Topic;
 
   ngOnInit() {
+    this.initializeTopic();
   }
 
+  initializeTopic = () => {
+    // dystopic axexexe
+    this.topic = new Topic;
+    this.topic.questions = [];
+  }
+
+  addTopicDisabled = (): boolean => {
+    return this.topic.questions.length < MIN_TOPIC_QUESTIONS_COUNT;
+  }
 }
