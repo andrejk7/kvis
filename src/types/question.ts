8f77549422f3e86eb19dfa5c90b6e4116bfbe449
Andrej Kolarovski <andrej@gsix.me>
2018-10-17 11:15:41 +0200

    added quiz class fields

--- src/types/question.ts
+++ src/types/question.ts
@@ -1,7 +1,10 @@
 export class Question {
   points: number;
+  bonusPoints?: number;
   question: string;
+  bonusQuestion?: string;
   questionImageEncoded?: string;
   answer: string;
+  bonusAnswer?: string;
   answerImageEncoded?: string;
 }
