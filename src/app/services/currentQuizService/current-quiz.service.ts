fb1fdc3d83eee05f63785a471587760824da55c5
Andrej Kolarovski <andrej@gsix.me>
2018-10-18 15:52:14 +0200

    mock quiz in current quiz service

--- src/app/services/currentQuizService/current-quiz.service.ts
+++ src/app/services/currentQuizService/current-quiz.service.ts
@@ -1,6 +1,42 @@
 import { Injectable } from '@angular/core';
 import { Quiz } from '../../../types/quiz';
 
+const mockQuiz = {
+  name: 'Trucks Quiz',
+  topics: [
+    {
+      name: 'trucks',
+      questions: [
+        {
+          points: 10,
+          question: 'Rubber Duck\'s truck?',
+          answer: 'Mack'
+        },
+        {
+          points: 20,
+          question: 'Jerry Reed\'s truck?',
+          answer: 'Kenworth w900'
+        },
+        {
+          points: 30,
+          question: 'Duel truck?',
+          answer: 'Peterbilt 351'
+        },
+        {
+          points: 40,
+          question: 'Ugliest Ukrainian truck?',
+          answer: 'KrAZ 255'
+        },
+        {
+          points: 50,
+          question: 'Dr. alcohol. Kolaroff concert truck?',
+          answer: 'DAF XF'
+        }
+      ]
+    },
+  ]
+};
+
 @Injectable()
 export class CurrentQuizService {
   private quiz: Quiz;
@@ -10,6 +46,6 @@ export class CurrentQuizService {
   }
 
   getQuiz = (): Quiz => {
-    return this.quiz;
+    return this.quiz || mockQuiz;
   }
 }
