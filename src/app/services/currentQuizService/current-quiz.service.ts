3906f8f7896eaecde2273b3f202917a9b36c5f80
Andrej Kolarovski <andrej@gsix.me>
2018-10-19 15:06:10 +0200

    updating mock data

--- src/app/services/currentQuizService/current-quiz.service.ts
+++ src/app/services/currentQuizService/current-quiz.service.ts
@@ -2,33 +2,40 @@ import { Injectable } from '@angular/core';
 import { Quiz } from '../../../types/quiz';
 
 const mockQuiz = {
+  id: 0,
   name: 'Trucks Quiz',
   isStoredOnDisc: false,
   topics: [
     {
+      id: 0,
       name: 'trucks',
       questions: [
         {
+          id: 0,
           points: 10,
           question: 'Rubber Duck\'s truck?',
           answer: 'Mack'
         },
         {
+          id: 1,
           points: 20,
           question: 'Jerry Reed\'s truck?',
           answer: 'Kenworth w900'
         },
         {
+          id: 2,
           points: 30,
           question: 'Duel truck?',
           answer: 'Peterbilt 351'
         },
         {
+          id: 3,
           points: 40,
           question: 'Ugliest Ukrainian truck?',
           answer: 'KrAZ 255'
         },
         {
+          id: 4,
           points: 50,
           question: 'Dr. alcohol. Kolaroff concert truck?',
           answer: 'DAF XF'
