930e926443902d2faee6486f5d089b69759314fc
Andrej Kolarovski <andrej@gsix.me>
2018-10-24 11:02:56 +0200

    added navigating to question

--- src/app/components/question-preview/question-preview.component.ts
+++ src/app/components/question-preview/question-preview.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Question } from '../../../types/question';
 
 @Component({
@@ -6,6 +7,29 @@ import { Question } from '../../../types/question';
   templateUrl: './question-preview.component.html',
   styleUrls: ['./question-preview.component.css']
 })
-export class QuestionPreviewComponent {
+export class QuestionPreviewComponent implements OnInit {
   @Input() question: Question;
+
+  private topicId: number;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.topicId = params.id;
+    });
+  }
+
+  openManageQuestion = () => {
+    this.router.navigate([
+      '/manage-topic',
+      this.topicId,
+      'question',
+      this.question.id
+    ]);
+  }
 }
+
