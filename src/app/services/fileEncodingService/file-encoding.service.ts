f68acf0be6c9142665549788a140ae79ac1004e5
Andrej Kolarovski <andrej@gsix.me>
2018-10-30 14:03:59 +0100

    added question image previews, removed console log

--- src/app/services/fileEncodingService/file-encoding.service.ts
+++ src/app/services/fileEncodingService/file-encoding.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class FileEncodingService {
   generateDataUrl = (file: File, callback: Function) => {
-    console.log(file);
     const reader = new FileReader();
     reader.onload = () => callback(reader.result);
     reader.readAsDataURL(file);
